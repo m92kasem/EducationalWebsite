618b84317984bb4958d3794a303f4dd490b87a66
m92kasem <m92kasem@gmail.com>
2024-07-04 10:31:52 +0200

    Updated name of IGenericRepository interface

--- src/EducationalWebsite.Domain/Interfaces/IGenericRepository.cs
+++ src/EducationalWebsite.Domain/Interfaces/IGenericRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace EducationalWebsite.Domain.Interfaces
 {
-    public interface IGenericIRepository<T> where T : class
+    public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(Guid id);
