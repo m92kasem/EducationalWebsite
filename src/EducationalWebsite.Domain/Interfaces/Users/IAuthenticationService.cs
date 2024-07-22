fa9eb9a7e43dccdc365abe456e4a7b4f42d71122
m92kasem <m92kasem@gmail.com>
2024-07-22 05:27:49 +0200

    Update IAuthenticationService interface

--- src/EducationalWebsite.Domain/Interfaces/Users/IAuthenticationService.cs
+++ src/EducationalWebsite.Domain/Interfaces/Users/IAuthenticationService.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EducationalWebsite.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 
 namespace EducationalWebsite.Domain.Interfaces.Users
 {
     public interface IAuthenticationService
     {
+      Task<IdentityResult> RegisterUserAsync(ApplicationUser user, string password);
       Task<(SignInResult signInResult, string token)> LoginUserAsync(string email, string password);
       Task LogoutUserAsync();  
       Task<IdentityResult> ResetPasswordAsync(string email, string token, string newPassword);
       Task GeneratePasswordResetTokenAsync(string email);
+      Task<bool> UserExistsAsync(string email);
     }
 }
